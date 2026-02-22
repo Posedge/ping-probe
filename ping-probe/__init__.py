008abdcc84b0f7b89bd884129b806ed35922c6d8
Valentin <valentin.trifonov.94@gmail.com>
2026-02-22 15:16:46 +0100

    Perform ping using icmplib

--- ping-probe/__init__.py
+++ ping-probe/__init__.py
@@ -3,9 +3,12 @@ import pathlib
 import logging
 import dataclasses
 import asyncio
+import time
+from typing import List
 
 
 import yaml
+import icmplib
 
 
 CONFIG_PATH = pathlib.Path('config.yaml')
@@ -14,9 +17,42 @@ CONFIG_PATH = pathlib.Path('config.yaml')
 @dataclasses.dataclass
 class Target:
     type: str
-    target: str
+    address: str
     timeout_millis: int = 5000
     interval_millis: int = 15000
+
+
+async def ping(target: Target):
+    try:
+        result = await icmplib.async_ping(
+            target.address, count=1, timeout=target.timeout_millis/1000., privileged=False)
+    except icmplib.ICMPError:
+        logging.warning('Failed to ping %s', target.address)
+    else:
+        logging.debug(f'Ping {target.address}: rtt ms={result.min_rtt}, '
+                      f'response received={'yes' if result.packets_received == 1 else 'no'}')
+
+
+async def sleep_until(target_time):
+    now = time.time()
+    if target_time > now:
+        await asyncio.sleep(target_time - now)
+
+
+async def monitor_target(target: Target):
+    logging.info('Target: %s', target)
+    try:
+        while True:
+            interval_start = time.time()
+            await ping(target)
+            await sleep_until(interval_start + target.interval_millis/1000.)
+    except asyncio.CancelledError:
+        logging.info('Stopped monitoring %s', target.address)
+
+
+async def monitor(targets: List[Target]):
+    tasks = [asyncio.create_task(monitor_target(t)) for t in targets]
+    await asyncio.gather(*tasks)
 
 
 def read_config(config_path=CONFIG_PATH):
@@ -25,21 +61,6 @@ def read_config(config_path=CONFIG_PATH):
         sys.exit(1)
     with config_path.open('r') as fin:
         return yaml.safe_load(fin)
-
-
-async def monitor_target(target: Target):
-    logging.info('Target: %s', target)
-    try:
-        while True:
-            logging.debug('Monitoring %s', target.target)  # TODO
-            await asyncio.sleep(target.interval_millis / 1000.)
-    except asyncio.CancelledError:
-        logging.info('Stopped monitoring %s', target.target)
-
-
-async def monitor(targets):
-    tasks = map(lambda t: asyncio.create_task(monitor_target(t)), targets)
-    await asyncio.gather(*tasks)
 
 
 def main():
